4b4379b2f29872bd504743bfe5070c54940cd95f
Shreyas Patankar <49764433+Shreyas-Patankar@users.noreply.github.com>
2021-07-29 17:23:33 -0700

    smoothing library spectra and removing extraneous portions

--- data_arrange.py
+++ data_arrange.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
-import os
+from scipy.signal import savgol_filter as sgf
+
+smoothing_window = 5
+smoothing_order = 2
 
 def deriv(p, w):
     return p - np.roll(p,w)
@@ -9,11 +12,11 @@ folder = r''
 
 OS_metadata = pd.read_csv('ftir_metadata.csv')
 OS_library = pd.read_csv('ftir_library.csv')
-
+OS_library = OS_library[((OS_library['wavenumber']<2300) | (OS_library['wavenumber']>2400)) & (OS_library['wavenumber']>905)]
 
 OS_int = pd.DataFrame()
 OS_derivative = pd.DataFrame()
-wavenos = np.array(range(400,4000,1))
+wavenos = np.array(range(905,4000,1))
 OS_int['wavenos'] = wavenos
 OS_int.set_index('wavenos')
 
@@ -24,8 +27,9 @@ for name in OS_metadata['sample_name']:
     wn = OS_library[OS_library['sample_name']==name].sort_values(by='wavenumber')['wavenumber']
     sig = OS_library[OS_library['sample_name']==name].sort_values(by='wavenumber')['intensity']
     sigint = np.interp(wavenos,wn,sig)
+    sigint = sigint - np.min(sigint)
     OS_int[name] = sigint
-    der_sigint = deriv(sigint,1)
+    der_sigint = sgf(deriv(sigint,smoothing_window),smoothing_window,smoothing_order)
     OS_derivative[name] = der_sigint
 
 
